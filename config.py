a0882097a4ac0658b6926158ce4f2319384daed6
Andrey Borokin <106249804+exp-ext@users.noreply.github.com>
2022-08-11 12:14:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,18 @@
 # токен телеграмм бота, получить можно  https://tlgg.ru/BotFather
-# TOKEN = '5395922222:AAGVENnG3cYEqiBvE4G_y5sALE7KkyaFuY8'
-TOKEN = '5586724882:AAFh3utxGM8-_PvYpnVP5szWRPj1YMMyz2I'
+TOKEN = ''
 
 # ID семейного группового чата
-# CHAT_ID = '-1001777599892'
-CHAT_ID = '-1001672385370'
+CHAT_ID = ''
 
 # ID на сайте предоставления прогноза погоды
 # https://home.openweathermap.org/api_keys
-OW_API_ID = '46bcbd1d671a980f039b66823a1b3e94'
+OW_API_ID = ''
 
 # ID на https://yandex.ru/dev/maps/geocoder/
-YANDEX_GEO_API = 'dfbc282b-38dd-4d50-953b-d549bd437432'
+YANDEX_GEO_API = ''
 
 # ID детей для предоставления только детских анекдотов
-ID_CHILDREN = ['2018237767']
+ID_CHILDREN = ['']
 
 # ID куда будут приходить уведомлеия о пропуске времени
-ID_ADMIN = '225429268'
+ID_ADMIN = ''
