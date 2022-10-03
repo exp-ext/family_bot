51f310230782bdad628fd68ff5535f6f4e061416
exp-ext <ext77@yandex.ru>
2022-10-03 18:21:55 +0300

    add

---
+++ data/config.py
@@ -0,0 +1,23 @@
+from telebot import TeleBot
+
+# токен телеграмм бота, получить можно
+# https://tlgg.ru/BotFather
+TOKEN = ''
+
+# ID telegram семейного группового чата
+CHAT_ID = ''
+
+# ID на сайте предоставления прогноза погоды
+# https://home.openweathermap.org/api_keys
+OW_API_ID = ''
+
+# ID на https://yandex.ru/dev/maps/geocoder/
+YANDEX_GEO_API = ''
+
+# ID детей для предоставления только детских анекдотов
+ID_CHILDREN = ['']
+
+# ID admin in Telegram, куда будут приходить уведомлеия о пропуске времени
+ID_ADMIN = ''
+
+bot = TeleBot(TOKEN)
